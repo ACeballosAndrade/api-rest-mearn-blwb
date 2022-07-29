33e81a68eb7c972651e8dd5da031dc6580142ea9
anthonyCeballos00 <anthony.ceballos00@usc.edu.co>
2022-07-28 19:16:05 -0500

    Token y refresh Token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,15 @@
 import {User} from '../models/User.js';
 import jwt from 'jsonwebtoken';
+import { generateRefreshToken, generateToken } from '../utils/tokenManager.js';
 
 export const register = async(req, res) => {
     const {email, password} = req.body
     try {
-        const user = new User({email, password});
+        //Alternativa buscadopor email
+        let user = await User.findOne({email});
+        if (user) throw {code: 11000};
+
+        user = new User({email, password});
         await user.save();
 
         //jwt token
@@ -32,13 +37,59 @@ export const login = async(req, res) => {
             return res.status(403).json({error: "Contraseña incorrecta"});
 
         //Generar Token JWT
-        const token = jwt.sign({uid: user._id}, process.env.JWT_SECRET)
+        const {token, expiresIn} = generateToken(user.id)
+        generateRefreshToken(user.id, res)
 
-
-        return res.json({token});
+        return res.json({token, expiresIn});
 
     } catch (error) {
         console.log(error)
         return res.status(500).json({error: "Algo falló en el servidor"})
     }
-}
+};
+
+export const infoUser = async(req, res) => {
+
+    try {
+        const user = await User.findById(req.uid).lean()
+        return res.json({email: user.email, uid: user.id})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error: "error de server"})
+    }
+
+
+};
+
+export const refreshToken = (req, res) => {
+
+    console.log("Este es el req ...."+req)
+    try {
+    const refreshTokenCookie = req.cookies.refreshToken;
+       if(!refreshTokenCookie) throw new Error("No existe el refreshTokenCookie");
+
+       const {uid} = jwt.verify(refreshTokenCookie, process.env.JWT_REFRESH);
+       console.log("este es el uid "+ uid);
+       const {token, expiresIn} = generateToken(uid)
+
+       return res.json({token, expiresIn});
+
+    } catch (error) {
+        console.log("Error en auth.controller ......"+error)
+        const TokenVerificationErrors = {
+            "invalid signature": "La firma del JWT no es válida",
+            "jwt expired": "JWT expirado",
+            "invalid token": "Token inválido",
+            "No Bearer": "Utiliza el formato Bearer",
+            "jwt malformed": "JWT formato no válido"
+        }
+    
+        return res.status(401).send({error: TokenVerificationErrors[error.message]})
+    }
+
+};
+
+export const logout = (req, res) => {
+    res.clearCookie('refreshToken')
+    res.json({ok: true});
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,7 @@
 import{Router} from "express";
-import { login, register } from "../controllers/auth.controller.js";
 import { body } from "express-validator";
+import { infoUser, login, register, refreshToken, logout } from "../controllers/auth.controller.js";
+import { requireToken } from "../middlewares/requireToken.js";
 import { validationResultExpress } from "../middlewares/validationResultExpress.js";
 
 const router = Router()
@@ -14,5 +15,9 @@ router.post('/login',[
     body('email', "Formato de email incorrecto").trim().isEmail().normalizeEmail(),
     body("password", "Formato de Password Incorrecta").trim().isLength({min: 6})
 ], validationResultExpress, login);
+
+router.get('/protected', requireToken, infoUser);
+router.get("/refresh", refreshToken );
+router.get('/logout', logout)
 
 export default router;
